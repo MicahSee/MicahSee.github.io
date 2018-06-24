5dabea0993c594cc1fa5ae787f571e314f5fbd07
Micah See <micah1711@Micahs-MBP.T-mobile.com>
2018-06-24 09:03:22 -0500

    Update Website

---
+++ static/js/resize.js
@@ -0,0 +1,8 @@
+$(
+  $(window).resize(function() {
+    var description = $('.product-description');
+    var image = $('.product-image');
+    console.log(description.height());
+    console.log(image.height());
+  })
+)
